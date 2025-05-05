23141a86f6eec1b47e0a31ff2ff2579a763b2fd3
moriyaarami <158502203+moriyaarami@users.noreply.github.com>
2025-05-06 01:04:55 +0300

    Update exerciseCard.jsx

--- frontend/fitPlan/src/commponets/exerciseCard.jsx
+++ frontend/fitPlan/src/commponets/exerciseCard.jsx
@@ -1,6 +1,7 @@
 import { useEx } from "../hooks/useEx";
 
 function ExerciseCard({ exInfo, deleteFromPlan }) {
+    console.log(exInfo)
 
     const Exercises = useEx();
     const correctEx = Exercises.find((ex) => {
@@ -8,27 +9,17 @@ function ExerciseCard({ exInfo, deleteFromPlan }) {
     })
 
     return correctEx && <div className="card" style={{ width: "18rem" }}>
-        <div className="card-titel">
-            {correctEx.name}
-        </div>
-        <div className="card-subtitle mb-2 text-muted">
-            {correctEx.description}
-        </div>
-        <div className="card-text">
-            <p >Location: <span className="text-muted">{correctEx.location}</span></p>
-            <p>Equipment
-                : <span className="text-muted">{correctEx.equipment
-                }</span></p>
-            <p>Difficulty
-                : <span className="text-muted">{correctEx.difficulty
-                }</span></p>
-            <p>Category
-                : <span className="text-muted">{correctEx.category
-                }</span></p>
-        </div>
-        <div className="d-flex  justify-content-around">
-            <p className="card-text">Sets: {correctEx.sets}</p>
-            <p className="card-text">Reps: {correctEx.reps}</p>
+        <div className="card-body">
+            <h5 className="card-title">{correctEx.name}</h5>
+            <p className="card-text">{correctEx.description}</p>
+            <p className="card-text">Location: {correctEx.location}</p>
+            <p className="card-text">Equipment: {correctEx.equipment}</p>
+            <p className="card-text">Difficulty: {correctEx.difficulty}</p>
+            <p className="card-text">Category: {correctEx.category}</p>
+            <div className="d-flex  justify-content-around">
+                <p className="card-text">Sets: {exInfo.sets}</p>
+                <p className="card-text">Reps: {exInfo.reps}</p>
+            </div>
         </div>
         <button className="btn btn-danger m-4 " onClick={() => deleteFromPlan(exInfo)}>Delete</button>
     </div >
